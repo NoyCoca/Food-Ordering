8c41d8019edd54ff9251c64b33bcbde07621912b
NoyCoca <noycoca123@gmail.com>
2022-01-21 19:17:44 +0200

    connit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT || 8080
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -15,12 +15,18 @@ const foodTypeRouter = require('./routers/foodTypeRouter')
 db.on('error', () => {
     console.log('Connection error');
 }); 
-if (process.env.NODE_ENV === 'production') {
-    append.use(express.static('client/build'))
-}
 
 app.use('/api/user', userRouter)
 app.use('/api/food', foodTypeRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
+const PORT = process.env.PORT || 8080
 app.listen(PORT, ()=> console.log(`Server stared on port ${PORT}`))
