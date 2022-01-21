894fb4d3a00f65650e775397f6152639897c58ca
NoyCoca <noycoca123@gmail.com>
2022-01-21 18:13:41 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 const express = require('express');
 const cors = require('cors');
 const app = express();
